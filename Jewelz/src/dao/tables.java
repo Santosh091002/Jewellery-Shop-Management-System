977d4768541e3518d3f89357a3b2c1e526dc591d
Santosh091002 <naiks6230@gmail.com>
2024-09-04 17:23:17 +0530

    Uploading Files

---
+++ Jewelz/src/dao/tables.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author naiks
+ */
+public class tables {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement st = null;
+        try {
+            con = ConnectionProvider.getCon();
+            st = con.createStatement();
+            st.executeUpdate("insert into appuser values('Jewelz','0909','Active','SuperAdmin')");
+            JOptionPane.showMessageDialog(null, "Table Created Successfully");
+            
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        } finally {
+            try {
+                con.close();
+                st.close();
+            } catch (Exception e) {
+            }
+        }
+
+    }
+}
